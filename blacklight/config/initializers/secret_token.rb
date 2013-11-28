e48886523d0a680afabc909f550c4a1d89ff6d00
Sam Popowich <spopowich@gmail.com>
2013-11-28 09:41:14 -0700

    Install blacklight.

---
+++ blacklight/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blacklight::Application.config.secret_key_base = '364e1844cb74eac39c48be3ef0c9b67f2453a71cabb7336aeeb358497a92b6d775f8d697c8ef88e994ab9da7d6583a0055fc8820cb422f8a731bd489379648a2'
---
+++ blacklight/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blacklight::Application.config.session_store :cookie_store, key: '_blacklight_session'
