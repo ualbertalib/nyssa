1302ebbe183a15290d91df38331933ca9aab197b
Sam Popowich <spopowich@gmail.com>
2013-12-23 13:22:21 -0700

    Add sirsi record logic.

--- data_scripts/main.rb
+++ data_scripts/main.rb
@@ -3,13 +3,8 @@ require_relative("includes")
 
 puts "Usage: main.rb marcxml matchissn badissn holderr summary outfile" unless ARGV[0]
 
-marcxml_file = ARGV[0]
-matchissn = ARGV[1]
-badissn = ARGV[2]
-holdings_errors = ARGV[3]
-summary_holdings = ARGV[4]
-solr_xml = ARGV[5]
 
+def process_sfx_records(marcxml_file, matchissn, badissn, holdings_errors, summary_holdings, solr_xml)
 puts "#new"
 solr_record_set = SolrRecordSet.new(marcxml_file)
 puts "#matchissn: #{matchissn}"
@@ -22,3 +17,23 @@ puts "#summary_holdings"
 solr_record_set.add_summary_holdings(SummaryHoldings.new(File.open(summary_holdings)))
 puts "#to_solr"
 solr_record_set.to_solr(solr_xml)
+end
+
+def process_sirsi_records(sirsi_file, sirsi_outfile)
+sirsi_records = SirsiRecords.new(File.open(sirsi_file).read)
+sirsi_records.to_solr(sirsi_outfile)
+end
+
+if ARGV[0] == "sfx"
+marcxml_file = ARGV[1]
+matchissn = ARGV[2]
+badissn = ARGV[3]
+holdings_errors = ARGV[4]
+summary_holdings = ARGV[5]
+solr_xml = ARGV[6]
+process_sfx_records(marcxml_file, matchissn, badissn, holdings_errors, summary_holdings, solr_xml)
+else
+sirsi_file = ARGV[1]
+sirsi_outfile = ARGV[2]
+process_sirsi_records(sirsi_file, sirsi_outfile)
+end
