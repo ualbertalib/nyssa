0a8eaf63a0701f516218c6f7687b9a0fa6e8f904
Sam Popowich <spopowich@gmail.com>
2014-03-04 09:02:22 -0700

    Modified solr_records.rb

--- data_scripts/main.rb
+++ data_scripts/main.rb
@@ -1,39 +1,30 @@
 require("marc")
 require_relative("includes")
 
-puts "Usage: main.rb sfx/sirsi marcxml matchissn badissn holderr summary outfile"; exit unless ARGV[0]
+puts "Usage: main.rb sfx/sirsi infile targets outfile"; exit unless ARGV[0]
 
-def process_sfx_records(marcxml_file, matchissn, badissn, holdings_errors, summary_holdings, solr_xml)
-puts "#new"
-solr_record_set = SolrRecordSet.new(marcxml_file)
-puts "#matchissn: #{matchissn}"
-solr_record_set.match(File.open(matchissn).read)
-puts "#badissn"
-solr_record_set.add_bad_issns(badissn)
-puts "#holdings_errors"
-solr_record_set.add_holding_errors(HoldingErrors.new(File.open(holdings_errors).read))
-#puts "#summary_holdings"
-#solr_record_set.add_summary_holdings(SummaryHoldings.new(File.open(summary_holdings)))
-puts "#to_solr"
-solr_record_set.to_solr(solr_xml)
+def process_sfx_records(infile, targets, solr_xml)
+  puts "#new"
+  solr_record_set = SolrRecordSet.new(File.open(infile), File.open(targets))
+  puts "#to_solr"
+  solr_record_set.to_solr(solr_xml)
 end
 
 def process_sirsi_records(sirsi_file, sirsi_outfile)
-sirsi_records = SirsiRecords.new(File.open(sirsi_file).read)
-sirsi_records.to_solr(sirsi_outfile)
+  sirsi_records = SirsiRecords.new(File.open(sirsi_file).read)
+  sirsi_records.to_solr(sirsi_outfile)
 end
 
 if ARGV[0] == "sfx"
-marcxml_file = ARGV[1]
-matchissn = ARGV[2]
-badissn = ARGV[3]
-holdings_errors = ARGV[4]
-summary_holdings = ARGV[5]
-solr_xml = ARGV[6]
-puts "processing sfx records"
-process_sfx_records(marcxml_file, matchissn, badissn, holdings_errors, summary_holdings, solr_xml)
+  datafile = ARGV[1]
+  targets_file = ARGV[2]
+  solr_xml = ARGV[3]
+  puts datafile
+  puts targets_file
+  puts "processing sfx records"
+  process_sfx_records(datafile, targets_file, solr_xml)
 else
-sirsi_file = ARGV[1]
-sirsi_outfile = ARGV[2]
-process_sirsi_records(sirsi_file, sirsi_outfile)
+  sirsi_file = ARGV[1]
+  sirsi_outfile = ARGV[2]
+  process_sirsi_records(sirsi_file, sirsi_outfile)
 end
--- data_scripts/solr_record_set.rb
+++ data_scripts/solr_record_set.rb
@@ -7,7 +7,8 @@ class SolrRecordSet
     @targets = {}
     datafile.each_line do |line|
       if line.include?("|") then
-        sfx_object_id = line.split("|").first
+        sfx_object_id = line.split("|").first.strip
+	puts sfx_object_id
         issn = line.split("|")[1]
         eissn = line.split("|")[2]
         link = line.split("|")[3]
@@ -19,11 +20,35 @@ class SolrRecordSet
     end
     targetsfile.each_line do |line|
       sfx_object_id = line.split("|").first.strip
+      puts "targets:#{sfx_object_id}"
       title = line.split(": ").first.split("|").last.strip
       tgts = Array(line.split(": ").last.strip)
       temp = @set[sfx_object_id].merge({:title => title, :targets => tgts}) unless @set[sfx_object_id].nil?
       @set[sfx_object_id] = temp
    end
   end
+ 
+  def to_solr(outfile)
+    @set.each do |key, value|
+      puts xml_representation(key, value)
+    end
+  end
 
+  private
+  
+  def xml_representation(sfx_object_id, record)
+    xml_record = %[<doc><field name =\"id\">#{sfx_object_id}</field>
+                   <field name = \"ua_object_id\">#{sfx_object_id}</field>
+		   <field name = \"ua_title\">#{record[:title]}</field>
+		   <field name = \"ua_issnPrint\">#{record[:issn]}</field>
+		   <field name = \"ua_issnElectronic\">#{record[:eissn]}</field>
+		   <field name = \"ua_freeJournal\">#{record[:free]}</field>
+		   <field name = \"ua_catkey\">#{record[:titleID]}</field>
+		   <field name = \"ua_singleTarget\">#{record[:single_target]}</field>
+		   <field name = \"ua_not_updated\">#{record[:not_updated]}</field>
+		   <field name = \"ua_sirsi_date_statement">#{record[:sirsi_dates]}</field>
+		   <field name = \"ua_sfx_date_statement">#{record[:sfx_dates]}</field>
+		   <field name = \"ua_bad_dates">#{record[:bad_dates]}</field>
+		   </doc>]
+  end
 end
